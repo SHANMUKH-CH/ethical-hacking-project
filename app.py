5d48a2b039b12ae6612979556b752d34a735791e
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-12 06:49:33 +0530

    created index page with login and reg buttons

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+  #a static webpage with buttons to login page as well as register
+  return render_template('index.html')
 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/register')
+def reg():
+  return render_template('register.html')
+
+@app.route('/homepage')
+def hp():
+  return render_template('homepage.html')
+
+@app.route('/profilepage')
+def pp():
+  return render_template('profilepage.html')
+
+@app.route('/searchpage')
+def sp(foo):
+    return render_template('searchpage.html')
+  
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
