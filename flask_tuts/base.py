814c106003ae3400a6270fe342f299ddf9e3f4a5
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-12 06:31:35 +0530

    flask practice

--- flask_tuts/base.py
+++ flask_tuts/base.py
@@ -10,5 +10,4 @@ def index():
 def kitty_name(name):
     return render_template('kittty.html', name=name)
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
- 
+  app.run(host='127.0.0.1', port=8000, debug=True) 
