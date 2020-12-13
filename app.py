c996cbd5d96d5e32f5a3730d81a4d25319dfb5e2
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-13 17:47:09 +0530

    minor correction

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
   #a static webpage with buttons to login page as well as register
   return render_template('index.html')
-
+ 
 @app.route('/login')
 def login():
   return render_template('login.html')
