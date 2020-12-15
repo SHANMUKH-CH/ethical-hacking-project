c7126e3cf5d9370327570f3319ac397cfffcb906
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-15 13:17:15 +0530

    changed function arguments

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
   #a static webpage with buttons to login page as well as register
   return render_template('index.html')
- 
+
 @app.route('/login')
 def login():
   return render_template('login.html')
@@ -26,7 +26,7 @@ def pp():
   return render_template('profilepage.html')
 
 @app.route('/searchpage')
-def sp(foo):
+def sp():
   return render_template('searchpage.html')
 
 @app.route('/thank_you')
