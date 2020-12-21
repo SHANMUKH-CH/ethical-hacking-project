c7a01464e12dd0a018185da5992dee332c4f575b
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-21 14:49:40 +0530

    registration

--- finalproject/SOCIAL_BLOG/__init__.py
+++ finalproject/SOCIAL_BLOG/__init__.py
@@ -20,7 +20,9 @@ login_manager.login_view='users.login'
 
 
 from SOCIAL_BLOG.core.views import core
+from SOCIAL_BLOG.users.views import users
 from SOCIAL_BLOG.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
---
+++ finalproject/SOCIAL_BLOG/users/views.py
@@ -0,0 +1,30 @@
+#users/views.py
+#reg,login,logout,account update, posts
+from flask import render_template,url_for,flash,redirect,request,Blueprint
+from flask.signals import request_tearing_down
+from flask_login import login_user,current_user, logout_user, login_required
+from SOCIAL_BLOG import db
+from SOCIAL_BLOG.models import User, BlogPost
+from SOCIAL_BLOG.users.forms import RegisterForm, loginForm, UpdateUserForm
+from SOCIAL_BLOG.users.picture_handler import add_profile_pic
+
+users =Blueprint('users',__name__)
+#logout
+@users.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("core.index"))
+
+#register
+@users.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, 
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("yo thanks for registration!")
+        return redirect(url_for('users.login'))
+    return render_template('register.html',form=form)
