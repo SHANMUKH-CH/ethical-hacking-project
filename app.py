a159004046d85cace5c9740c4ac75f269cea8a2a
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-21 20:16:21 +0530

    password check

--- app.py
+++ app.py
@@ -31,30 +31,35 @@ def login():
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = message)
 
+
+
 @app.route('/register', methods =['GET', 'POST']) 
 def register(): 
     msg = '' 
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form : 
         username = request.form['username'] 
-        password = request.form['password'] 
+        password = request.form['password']
+        confirm = request.form['confrim']
         email = request.form['email'] 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
         cursor.execute('SELECT * FROM registration WHERE username = % s', (username, )) 
-        account = cursor.fetchone() 
+        account = cursor.fetchone()
         if account: 
-            msg = 'Account already exists !'
+            msg = 'Bruhhh Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
-            msg = 'Invalid email address !'
+            msg = 'Bruhhh Invalid email address !'
+        elif password != confirm:
+            msg = 'Bruhhh Password mismatch !'
         elif not re.match(r'[A-Za-z0-9]+', username): 
             msg = 'Username must contain only characters and numbers !'
         elif not username or not password or not email: 
-            msg = 'Please fill out the form !'
+            msg = 'Bruhhh Please fill out the form !'
         else: 
-            cursor.execute('INSERT INTO registration VALUES (NULL, % s, % s, % s)', (username, password, email, )) 
+            cursor.execute('INSERT INTO registration VALUES (NULL, % s, % s, % s)', (username, email, password )) 
             mysql.connection.commit() 
             msg = 'You have successfully registered !'
     elif request.method == 'POST': 
-        msg = 'Please fill out the form !'
+        msg = 'Bruhh Please fill out the form !'
     return render_template('register.html', msg = msg)
 
 @app.route('/logout') 
