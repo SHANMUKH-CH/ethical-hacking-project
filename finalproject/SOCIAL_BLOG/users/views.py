5ee20c8c70688d89071e255eef1d5e98e91a4cdd
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-21 15:15:03 +0530

    finale

--- finalproject/SOCIAL_BLOG/users/views.py
+++ finalproject/SOCIAL_BLOG/users/views.py
@@ -3,6 +3,7 @@
 from flask import render_template,url_for,flash,redirect,request,Blueprint
 from flask.signals import request_tearing_down
 from flask_login import login_user,current_user, logout_user, login_required
+from sqlalchemy.util.compat import u
 from SOCIAL_BLOG import db
 from SOCIAL_BLOG.models import User, BlogPost
 from SOCIAL_BLOG.users.forms import RegisterForm, loginForm, UpdateUserForm
@@ -28,3 +29,19 @@ def register():
         flash("yo thanks for registration!")
         return redirect(url_for('users.login'))
     return render_template('register.html',form=form)
+
+#login
+@users.route('/login', methods=['GET','POST'])
+def login():
+    form = loginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash('Log in success!')
+            next= request.args.get('next')
+            if next == None or next[0]=='/':
+                next = url_for('core.index')
+            return redirect(next)
+        return render_template('login.html',form=form)
